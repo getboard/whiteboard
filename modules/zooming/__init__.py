ce40cac4f6ba3fe7e2c9efb33b3e33ffb7628427
Anton Dobrynin <hashlib@yandex.ru>
2023-03-25 11:41:40 +0300

    Turn on zooming back

--- modules/zooming/__init__.py
+++ modules/zooming/__init__.py
@@ -5,13 +5,12 @@ from . import handlers
 
 
 def bind_on_events(ctx: context.Context):
-    # ctx.canvas.bind('<Control-MouseWheel>',
-    #                 lambda event: handlers.zoom_on_wheel(ctx, event))  # Win + MacOS
-    # ctx.canvas.bind('<Control-Button-5>',
-    #                 lambda event: handlers.zoom_on_wheel(ctx, event))  # Linux
-    # ctx.canvas.bind('<Control-Button-4>',
-    #                 lambda event: handlers.zoom_on_wheel(ctx, event))  # Linux
-    pass  # TODO
+    ctx.canvas.bind('<Control-MouseWheel>',
+                    lambda event: handlers.zoom_on_wheel(ctx, event))  # Win + MacOS
+    ctx.canvas.bind('<Control-Button-5>',
+                    lambda event: handlers.zoom_on_wheel(ctx, event))  # Linux
+    ctx.canvas.bind('<Control-Button-4>',
+                    lambda event: handlers.zoom_on_wheel(ctx, event))  # Linux
 
 
 @modules.modules.register_module('zooming')
