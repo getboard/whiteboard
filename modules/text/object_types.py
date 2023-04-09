f7b24265739f1215d12941ad7f0e9cf91176f959
Anton <hashlib@yandex.ru>
2023-04-09 18:04:59 +0300

    Fixed codestyle

--- modules/text/object_types.py
+++ modules/text/object_types.py
@@ -16,13 +16,11 @@ class TextObject(objects_storage.Object):
         self._width = 100
         self._ids = None
         self.last_clicked = False  # ��������� ������ ������ ���� �����
-        self._text_id = ctx.canvas.create_text(kwargs['x'],
-                                               kwargs['y'],
-                                               text=kwargs['text'],
-                                               tags=[id, 'text'],
-                                               font=self.get_font())
+        self._text_id = ctx.canvas.create_text(
+            kwargs['x'], kwargs['y'], text=kwargs['text'], tags=[id, 'text'], font=self.get_font()
+        )
 
-    def update(self,  ctx: context.Context, **kwargs):
+    def update(self, ctx: context.Context, **kwargs):
         ctx.canvas.itemconfig(self.id, **kwargs)
 
     def get_font(self):
@@ -38,19 +36,23 @@ class TextObject(objects_storage.Object):
         text = ctx.canvas.itemcget(self._text_id, 'text')
         return text
 
-    def scale(self,  ctx: context.Context,  scale_factor: float):
+    def scale(self, ctx: context.Context, scale_factor: float):
         self._font_size *= scale_factor
         self._width *= scale_factor
         ctx.canvas.itemconfig(self._text_id, font=self.get_font())
 
-    def highlight(self,  ctx: context.Context):
+    def highlight(self, ctx: context.Context):
         # items = self._ctx.canvas.find(self._ids)
         # self._ctx.canvas.tag_bind(self._text_id, '<FocusOut>', self.focus_out)
         if self._ids not in ctx.canvas.find_all():
             ctx.canvas.delete('highlight')
-            self._ids = ctx.canvas.create_rectangle((0, 0, 0, 0), fill='white',
-                                                    outline='blue',
-                                                    dash='.', tags=[self.id, 'text', 'highlight'])
+            self._ids = ctx.canvas.create_rectangle(
+                (0, 0, 0, 0),
+                fill='white',
+                outline='blue',
+                dash='.',
+                tags=[self.id, 'text', 'highlight'],
+            )
             ctx.canvas.lower(self._ids, self._text_id)
         else:
             # ids = items[0]
--- state_machine.py
+++ state_machine.py
@@ -88,10 +88,8 @@ class StateMachine:
         self._global_context.canvas.bind('<ButtonPress-1>', self.handle_event)
         self._global_context.canvas.bind('<B1-Motion>', self.handle_event)
         self._global_context.canvas.bind('<Key>', self.handle_event)
-        self._global_context.canvas.bind(
-            '<Shift-ButtonPress-1>', self.handle_event)
-        self._global_context.canvas.bind(
-            '<ButtonRelease-1>', self.handle_event)
+        self._global_context.canvas.bind('<Shift-ButtonPress-1>', self.handle_event)
+        self._global_context.canvas.bind('<ButtonRelease-1>', self.handle_event)
         # TODO: add more binds
 
     def add_state(self, state: State):
@@ -117,14 +115,11 @@ class StateMachine:
                     # Залоггировать ошибку
                     return
                 # Залоггировать, что выходим из состояния before
-                self._cur_state.on_leave(
-                    self._global_context, self._cur_state_context, event)
+                self._cur_state.on_leave(self._global_context, self._cur_state_context, event)
                 self._cur_state_context = self._make_empty_context()
                 self._cur_state = after_state
                 # Залоггировать, что входим в состояние after
-                self._cur_state.on_enter(
-                    self._global_context, self._cur_state_context, event)
+                self._cur_state.on_enter(self._global_context, self._cur_state_context, event)
                 return
         # Залоггировать, что ни один предикат не выполнился
-        self._cur_state.handle_event(
-            self._global_context, self._cur_state_context, event)
+        self._cur_state.handle_event(self._global_context, self._cur_state_context, event)
