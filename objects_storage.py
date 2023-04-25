988e2fa43fe9e1c67177aee4722830e5087d8ca2
Anton <hashlib@yandex.ru>
2023-04-25 19:54:44 +0300

    Fixed objects_storage

--- objects_storage.py
+++ objects_storage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import annotations
 import uuid
 from typing import Type
@@ -73,74 +72,3 @@ class ObjectsStorage:
 
     def update(self, object_id: str, **kwargs):
         self._objects[object_id].update(self._ctx, **kwargs)
-=======
-from __future__ import annotations
-import uuid
-from typing import Type
-from typing import Optional
-
-import context
-
-
-class Object:
-    id: str
-
-    def __init__(self, ctx: context.Context, id: str):
-        self.id = id
-
-    def move(self, ctx: context.Context, delta_x: int, delta_y: int):
-        ctx.canvas.move(self.id, delta_x, delta_y)
-
-    def move_to(self, ctx: context.Context, x: int, y: int):
-        ctx.canvas.moveto(self.id, x, y)
-
-    def update(self, ctx: context.Context, **kwargs):
-        raise NotImplementedError("it's an abstract class")
-
-    def scale(self, ctx: context.Context, scale_factor: float):
-        raise NotImplementedError("it's an abstract class")
-
-
-class ObjectsStorage:
-    _ctx: context.Context
-    _objects: dict[str, Object]
-    _object_types: dict[str, Type[Object]]
-
-    def __init__(self, ctx: context.Context):
-        self._ctx = ctx
-        self._objects = {}
-        self._object_types = {}
-
-    def register_object_type(self, type_name: str, type_class: Type[Object]):
-        self._object_types[type_name] = type_class
-
-    def get_by_id(self, object_id: str) -> Object:
-        return self._objects[object_id]
-
-    def get_opt_by_id(self, object_id: str) -> Optional[Object]:
-        return self._objects.get(object_id)
-
-    def get_current_opt(self) -> Optional[Object]:
-        tags = self._ctx.canvas.gettags('current')
-        if not tags:
-            return None
-        return self.get_opt_by_id(tags[0])
-
-    def get_current_opt_type(self) -> str:
-        tags = self._ctx.canvas.gettags('current')
-        if not tags or len(tags) < 2:
-            return ''
-
-        return tags[1]
-
-    def get_objects(self) -> dict[str, Object]:
-        return self._objects
-
-    def create(self, type_name: str, **kwargs) -> str:
-        obj_id = kwargs.get('obj_id', uuid.uuid4().hex[:10])
-        self._objects[obj_id] = self._object_types[type_name](self._ctx, obj_id, **kwargs)
-        return obj_id
-
-    def update(self, object_id: str, **kwargs):
-        self._objects[object_id].update(self._ctx, **kwargs)
->>>>>>> Fixed codestyle
