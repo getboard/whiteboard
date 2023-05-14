ff4180e15ccac86454224e6929cae0bdc8c4e1bc
Anton <hashlib@yandex.ru>
2023-05-14 15:05:20 +0300

    Fixed text deletion in text and sticker

--- modules/sticker/states/edit_sticker.py
+++ modules/sticker/states/edit_sticker.py
@@ -44,7 +44,7 @@ def _handle_event(global_ctx: 'Context', state_ctx: Dict, event: tkinter.Event):
     if event.keysym == 'BackSpace':
         insert = global_ctx.canvas.index(cur_obj.get_text_id(), 'insert')
         if insert > 0:
-            global_ctx.canvas.dchars(cur_obj.get_text_id(), insert - 1, insert)
+            global_ctx.canvas.dchars(cur_obj.get_text_id(), insert - 1, insert - 1)
         cur_obj.adjust_font(global_ctx, False)
         return
 
