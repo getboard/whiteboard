bdb493561a6a37bd0c9f07ffc071e24781ed7e84
Anton <43320720+aedobrynin@users.noreply.github.com>
2023-05-17 21:34:11 +0300

    Added pub_sub

--- context.py
+++ context.py
@@ -8,7 +8,7 @@ from event_handlers import EventHandlers
 from menu import Menu
 from objects_storage import ObjectsStorage
 from state_machine import StateMachine
-
+from pub_sub import Broker
 
 class Context:
     events_history: EventsHistory
@@ -19,3 +19,4 @@ class Context:
     state_machine: StateMachine
     property_bar: ttk.Frame
     menu: Menu
+    pub_sub_broker: Broker
