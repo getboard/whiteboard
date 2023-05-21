eeca00f2f31f41fc4977ad10eed1fb2dde3f3f27
Anton <hashlib@yandex.ru>
2023-05-21 21:43:25 +0300

    Save events as json

--- events_history.py
+++ events_history.py
@@ -1,33 +1,28 @@
 from __future__ import annotations
-import time
 import os
+import json
 
 import context
 
 
+__KIND_FIELD_NAME = 'kind'
+
+
 class EventInfo:
-    timestamp: int
     kind: str
     kwargs: dict
 
     @classmethod
-    def parse(cls, raw: str):
-        kwargs = {}
-        for raw_pair in raw.split(';'):
-            key, value = raw_pair.split('=')
-            kwargs[key] = value
-
+    def from_payload(cls, payload: dict) -> EventInfo:
         event_info = EventInfo()
-        event_info.timestamp = int(kwargs.pop('timestamp'))
-        event_info.kind = kwargs.pop('kind')
-        event_info.kwargs = kwargs
+        event_info.kind = payload.pop(__KIND_FIELD_NAME)
+        event_info.kwargs = payload
         return event_info
 
-    def serialize(self) -> str:
-        res = f'timestamp={self.timestamp};kind={self.kind}'
-        if self.kwargs:
-            res += ';' + ';'.join([f'{key}={value}' for key, value in self.kwargs.items()])
-        return res
+    def get_payload(self):
+        payload = {__KIND_FIELD_NAME: self.kind}
+        payload.update(self.kwargs)
+        return payload
 
 
 class EventsHistory:
@@ -38,22 +33,25 @@ class EventsHistory:
 
     def add_event(self, kind: str, **event_kwargs):
         event_info = EventInfo()
-        event_info.timestamp = int(time.time())
         event_info.kind = kind
         event_info.kwargs = event_kwargs
         self._events.append(event_info)
 
     def save_to_file(self, path: str):
         with open(path, 'w') as file:
+            payloads = []
             for event in self._events:
-                file.write(event.serialize() + '\n')
+                payloads.append(event.get_payload())
+            json.dump(payloads, file, ensure_ascii=False, indent=None)
 
     def load_from_file_and_apply(self, ctx: context.Context, path: str):
         if not os.path.exists(path) or not os.path.isfile(path):
             return
+
         with open(path, 'r') as file:
-            for line in file:
-                event_info = EventInfo.parse(line.strip())
+            event_payloads = json.load(file)
+            for payload in event_payloads:
+                event_info = EventInfo.from_payload(payload)
                 self._events.append(event_info)
                 handler = ctx.event_handlers.get_handler(event_info.kind)
                 handler.apply(ctx, **event_info.kwargs)
